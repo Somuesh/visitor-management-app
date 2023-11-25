39818b34a2c283fcd3d7ac127c5586661924b001
Somesh Sharma <sharmsom@amazon.com>
2023-11-26 00:32:36 +0530

    fields not empty validation and frontend configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
 // import Navbar from './components/navbar/Navbar';
 import LandingPage from './components/landingPage/LandingPage';
 
