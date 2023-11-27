40a9b4075b38d0070f87b8798d0eaa89049dcc85
Somesh Sharma <sharmsom@amazon.com>
2023-11-27 21:22:36 +0530

    backend endpoints configured

---
+++ server/staff.js
@@ -0,0 +1,36 @@
+const staff = [
+    {
+        id: 1,
+        name: 'Staff 1',
+        department: 'HR',
+        image: './island.png',
+        email: 'staff-email@techdunia.com',
+        mobile: '9876678901'
+    },
+    {
+        id: 2,
+        name: 'Staff 2',
+        department: 'Tech',
+        image: 'image2.jpeg',
+        email: 'staff2-email@techdunia.com',
+        mobile: '9876678902'
+    },
+    {
+        id: 3,
+        name: 'Staff 3',
+        department: 'Tech',
+        image: 'image3.jpeg',
+        email: 'staff3-email@techdunia.com',
+        mobile: '9876678903'
+    },
+    {
+        id: 4,
+        name: 'Staff 4',
+        department: 'Finance',
+        image: 'image4.jpeg',
+        email: 'staff4-email@techdunia.com',
+        mobile: '9876678904'
+    }
+]
+
+module.exports = {staff};
